1d6e9dc9d056e86070820379002082cdfc6694c7
Muris Ceman <murisceman@gmail.com>
2022-08-12 21:32:01 +0200

    Init

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'ignore',
+  printWidth: 100,
+  quoteProps: 'consistent',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+}
