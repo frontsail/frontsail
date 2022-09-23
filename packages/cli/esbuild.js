261d905a0609158a8cff95ac7e58615421240d76
Muris Ceman <murisceman@gmail.com>
2022-09-23 19:07:05 +0200

    Organize dependencies

--- packages/cli/esbuild.js
+++ packages/cli/esbuild.js
@@ -7,7 +7,14 @@ esbuild.build({
   bundle: true,
   platform: 'node',
   mainFields: ['module', 'main'],
-  external: ['@parcel/watcher', 'browser-sync', 'esbuild', 'fsevents', 'prettier'],
+  external: [
+    '@parcel/watcher',
+    'browser-sync',
+    'eastasianwidth',
+    'esbuild',
+    'fsevents',
+    'prettier',
+  ],
   minify: !process.argv.includes('--watch'),
   watch: process.argv.includes('--watch'),
 })
