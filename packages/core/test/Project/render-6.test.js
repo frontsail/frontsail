06375d508be9a21a2b6dba5008f24440b47c953a
Muris Ceman <murisceman@gmail.com>
2022-09-29 16:56:59 +0200

    Update render tests

--- packages/core/test/Project/render-6.test.js
+++ packages/core/test/Project/render-6.test.js
@@ -16,6 +16,6 @@ test('rendering (production)', () => {
 
   expect(results.diagnostics).toHaveLength(0)
   expect(results.html).toBe(
-    '<div x-data="_c1_D" data-foo="1"><template x-bind="_c1b1_D"><template x-for="bar in 10"><div><span x-bind="_c1b2_D"></span></div></template></template></div>',
+    '<div x-data="_c1_D" data-foo="1"><template hidden="" x-bind="_c1b1_D"><template x-for="bar in 10" hidden=""><div><span x-bind="_c1b2_D"></span></div></template></template></div>',
   )
 })
